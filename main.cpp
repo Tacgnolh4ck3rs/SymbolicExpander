bcf44f79520ff9f6bbb198e4d1a97a5c63518b90
Saiba96 <salvador.calderon.131@gmail.com>
2020-12-13 23:51:29 -0600

    update json/main file (again)😅📃

--- main.cpp
+++ main.cpp
@@ -2,22 +2,27 @@
 #include <iostream>
 #include "json.hpp"
 #include <fstream>
-
+#include <vector>
 using json=nlohmann::json;
 using namespace std;
 
 int main(int argc, char **argv) {
-    json Din;
-    ifstream user("user.json");
-    user>>Din;
-    string functionStr=Din["function"];
-    double rangeMin=Din["range"]["min"];
-    double rangeMax=Din["range"]["max"];
-    int partitions=Din["partitions"];
-    cout<<"Function: ("<<functionStr<<"), Range: ["<<
-        rangeMin<<" - "<< rangeMax<<"]. Partitions: "<<
-        partitions<<endl;
-
-
+    
+    vector<string> arrayValue;
+    ifstream problem("config.json");
+    json file=json::parse(problem);
+    
+    for(json& o: file["problems"]){
+    json& gi=o;
+    arrayValue.push_back(gi["name"]);
+    cout<<"name: "<<gi["name"]<<endl;
+    arrayValue.push_back(gi["expr"]);
+    cout<<"expresion: "<<gi["expr"]<<endl;
+    arrayValue.push_back(gi["operation"]["code"]);
+    cout<<"code: "<<gi["operation"]["code"]<<endl;
+    }
+    //for(int pos=0; arrayValue.size();pos++)
+    //cout<<arrayValue.at(pos)<<endl;
+    
     return 0;
 }
