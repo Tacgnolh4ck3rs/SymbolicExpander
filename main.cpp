79e0bae291fd274f8d6665adf55e78042a93a408
Saiba96 <salvador.calderon.131@gmail.com>
2020-12-18 21:49:55 -0600

    main updates📃

--- main.cpp
+++ main.cpp
@@ -1,28 +1,42 @@
+//Standard headers
 #include <string>
 #include <iostream>
-#include "json.hpp"
 #include <fstream>
 #include <vector>
+#include <map>
+//Additional headers
+#include "json.hpp"
+#include "argh.h"
+//#include "symboliccpp/symbolicc++.h"
+//SymbolicC++ headers
+
 using json=nlohmann::json;
 using namespace std;
 
 int main(int argc, char **argv) {
-    
+    //SymEngine::print_stack_on_segfault();
+    //RCP<const Basic> x= symbol("x");
+    argh::parser cmdl;
+    cmdl.add_params({"-input", "--fileName"});//Argh! input 
+    cmdl.parse(argc,argv);
+    string fileName = cmdl({"-input","--fileNme"}).str();
     vector<string> arrayValue;
-    ifstream problem("config.json");
-    json file=json::parse(problem);
-    
-    for(json& o: file["problems"]){
-    json& gi=o;
-    arrayValue.push_back(gi["name"]);
-    cout<<"name: "<<gi["name"]<<endl;
-    arrayValue.push_back(gi["expr"]);
-    cout<<"expresion: "<<gi["expr"]<<endl;
-    arrayValue.push_back(gi["operation"]["code"]);
-    cout<<"code: "<<gi["operation"]["code"]<<endl;
-    }
-    //for(int pos=0; arrayValue.size();pos++)
-    //cout<<arrayValue.at(pos)<<endl;
-    
+    //Key type is string
+    //           value type is vector<string>
+    //map< KeyType, ValueType > myMap;
+    map< string, vector<string> > myMap;
+    ifstream problem(fileName);//ifstream problem("config.json");
+    json file=json::parse(problem);//json parsing
+
+    for(json& o: file["problems"])
+        myMap.insert(pair<string, vector<string> >(o["name"], {
+        o["expr"],
+        o["operation"]["code"]
+    }));
+    for (auto k : myMap)
+        cout <<
+             "Problem name: " << k.first << endl <<
+             "Expression: " << k.second.at(0) << endl <<
+             "Operation code: " << k.second.at(1) << endl;
     return 0;
 }
