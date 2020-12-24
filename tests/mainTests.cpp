b8d0998d783cfe4452315d368c281d895ed1b55c
thekingisbackboys-code <gmogro4@hotmail.com>
2020-12-23 18:01:27 -0600

    Tests fixed

--- tests/mainTests.cpp
+++ tests/mainTests.cpp
@@ -1,6 +1,20 @@
 #include <gtest/gtest.h>
-#include "../utils.hpp"
 
+TEST(NaiveTests, FirstTest) {
+    EXPECT_EQ(1, 1);
+}
+
+TEST(NaiveTests, SecondTest) {
+    EXPECT_EQ(2, 2);
+}
+
+TEST(NaiveTests, ThirdTest) {
+    EXPECT_EQ(0, 0);
+}
+
+TEST(OtherTest, FirstTest) {
+    EXPECT_EQ(1, 1);
+}
 
 
 
