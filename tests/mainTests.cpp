b56319be36032f13355cdf600a5d586ca77d998f
thekingisbackboys-code <gmogro4@hotmail.com>
2021-01-09 14:44:11 -0600

    Modified tests so now they could run
    Some SetUpTestSuite was required

--- tests/mainTests.cpp
+++ tests/mainTests.cpp
@@ -4,48 +4,53 @@
 #include <iostream>
 #include <cstring>
 
-
+/*
 class BaseTest : public ::testing::Test{
 };
 
-// Derives a fixture FooTest from BaseTest.
-class FooTest : public BaseTest {
-protected:
 
-    void SetUp() override {
+*/
+// Derives a fixture FooTest from BaseTest.
+//class FooTest : public BaseTest {
+class FooTest : public ::testing::Test {
+protected:
+    static expSolve *thing;
+    static void SetUpTestSuite() {
         //SetUp();  // Sets up the base fixture first.
+        thing = new expSolve();
         std::cout<<"SetUp called :) \n";
     }
 
-    void TearDown() override {
+    static void TearDownTestSuite() {
         //TearDown();  // Remember to tear down the base fixture
         // after cleaning up FooTest!
+        delete thing;
         std::cout<<"TearDown called :(\n";
     }
-
-    expSolve thing;
+    virtual ~FooTest() noexcept override {};
 };
+
+expSolve *FooTest::thing;
 
 //-------------------------------------
 //Error code testing
 TEST_F(FooTest, FirstTestCode) {
     //expSolve thing;
     std::string str("Expand the expression:");
-    EXPECT_EQ(str.c_str(),std::get<0>(thing.select("expand")));
-    thing.~expSolve();
+    EXPECT_EQ(str.c_str(),std::get<0>(thing->select("expand")));
 }
 
 TEST_F(FooTest, SecondTestCode) {
     //expSolve thing;
     std::string str("Expand the expression:");
-    EXPECT_EQ(str.c_str(),std::get<0>(thing.select("Expand")));
+    EXPECT_EQ(str.c_str(),std::get<0>(thing->select("Expand")));
 }
 
 
 TEST_F(FooTest, ThirdTestCode) {
     //expSolve thing;
     std::string str("Unknown operation code \"");
-    EXPECT_EQ(str.c_str(),std::get<0>(thing.select("I have an existential crisis")));
+    EXPECT_EQ(str.c_str(),std::get<0>(thing->select("I have an existential crisis")));
 }
 //--------------------------------------
 
@@ -56,18 +61,11 @@ TEST_F(FooTest, ThirdTestCode) {
 TEST_F(FooTest, EZPZExp) { //(x+1)*(x+2)*(x+3) --> x**3 + 6*x**2 + 11*x + 6
     //expSolve thing;
     std::string str("x**3 + 6*x**2 + 11*x + 6");
-    EXPECT_EQ(str.c_str(), thing.pyExpand("(x+1)*(x+2)*(x+3)"));
+    EXPECT_EQ(str.c_str(), thing->pyExpand("(x+1)*(x+2)*(x+3)"));
 }
 
 TEST_F(FooTest, KindaComplexAfButWhoReallyKnowsExp) { //(x+1)/(x+2)**2 * (x+3) --> x**2/(x**2 + 4*x + 4) + (4*x)/(x**2 + 4*x + 4) + 3/(x**2 + 4*x + 4)
     //expSolve thing;
     std::string str("x**2/(x**2 + 4*x + 4) + (4*x)/(x**2 + 4*x + 4) + 3/(x**2 + 4*x + 4)");
-    EXPECT_EQ(str.c_str(), thing.pyExpand("(x+1)/(x+2)**2 * (x+3)"));
+    EXPECT_EQ(str.c_str(), thing->pyExpand("(x+1)/(x+2)**2 * (x+3)"));
 }
-/*
-int main(int argc, char* argv[])
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
-*/
